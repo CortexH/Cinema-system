3eaa0504c9b0fca29b8c6514459d8ca87e357bcf
CortexH <henrique.silveira.soares19988@gmail.com>
2025-07-16 16:53:22 -0300

    teste do 'caminho feliz' do endpoint de inserção de sessões

--- scheduling-service/src/test/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/ScheduledSessionsIntegrationTests.java
+++ scheduling-service/src/test/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/ScheduledSessionsIntegrationTests.java
@@ -1,14 +1,34 @@
 package com.example.scheduling_service.infrastructure.adapter.inbound.web;
 
+import com.example.scheduling_service.application.dto.request.SessionRequestDTO;
+import com.example.scheduling_service.domain.enums.SessionScheduleState;
+import com.example.scheduling_service.domain.model.Session;
+import com.example.scheduling_service.domain.valueObject.SessionIdVO;
+import com.example.scheduling_service.infrastructure.adapter.inbound.web.mapper.SessionRequestDTOMapper;
+import com.example.scheduling_service.infrastructure.adapter.outbound.persistence.entity.SessionEntity;
+import com.example.scheduling_service.infrastructure.adapter.outbound.persistence.mapper.SessionMapper;
+import com.example.scheduling_service.infrastructure.adapter.outbound.persistence.repository.repository.SessionRepositoryJPA;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
@@ -17,14 +37,40 @@ public class ScheduledSessionsIntegrationTests {
 
     private final MockMvc mockMvc;
     private final ObjectMapper objectMapper;
+    private final SessionRepositoryJPA sessionRepository;
 
     @Test
-    @DisplayName("Deverá inserir uma sessão de teste e removê-la")
+    @DisplayName("Deverá inserir uma sessão com sucesso")
     @Transactional
-    void testRemoveSession(){
+    void testInsertSession() throws Exception {
 
+        SessionRequestDTO data = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                "2025-07-16T14:00:00",
+                "2025-07-16T17:00:00",
+                2700L, 540000L
+        );
 
+        Session requestSession = SessionRequestDTOMapper.toInbound(data);
 
+        String request = objectMapper.writeValueAsString(data);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        Session databaseSession = sessionRepository.findAll()
+                .stream().map(SessionMapper::toInbound).toList().getFirst();
+
+        Assert.isTrue(databaseSession.getSessionBeginTime().equals(requestSession.getSessionBeginTime()), "o campo 'beginTime' de ambas as sessões não condizem");
+        Assert.isTrue(databaseSession.getSessionEndTime().equals(requestSession.getSessionEndTime()), "o campo 'endTime' de ambas as sessões não condizem");
+        Assert.isTrue(databaseSession.getMovieId().equals(requestSession.getMovieId()), "o campo 'movieId' de ambas as sessões não condizem");
+        Assert.isTrue(databaseSession.getRoomId().equals(requestSession.getRoomId()), "o campo 'roomId' de ambas as sessões não condizem");
+        Assert.isTrue(databaseSession.getSetupTime().equals(requestSession.getSetupTime()), "o campo 'setupTime' de ambas as sessões não condizem");
     }
+
+
 
 }
