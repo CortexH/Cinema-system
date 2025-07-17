2a3fb329712783a356bb43383a04c96a24187577
CortexH <henrique.silveira.soares19988@gmail.com>
2025-07-17 00:16:49 -0300

    Adição do 'sessionDisplayMapper'. É a classe responsável por mapear o session e mostrá-lo ao usuário.

--- scheduling-service/src/main/java/com/example/scheduling_service/application/dto/response/SessionDisplayDTO.java
+++ scheduling-service/src/main/java/com/example/scheduling_service/application/dto/response/SessionDisplayDTO.java
@@ -1,15 +1,15 @@
 package com.example.scheduling_service.application.dto.response;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record SessionDisplayDTO(
 
         UUID session_id,
         String room_id,
-        String movie_name,
         String movie_id,
-        String session_display_time,
-        String session_end_time
-
+        LocalDateTime session_begin_time,
+        LocalDateTime session_end_time,
+        Long movie_duration
 ) {
 }
---
+++ scheduling-service/src/main/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/mapper/SessionDisplayMapper.java
@@ -0,0 +1,19 @@
+package com.example.scheduling_service.infrastructure.adapter.inbound.web.mapper;
+
+import com.example.scheduling_service.application.dto.response.SessionDisplayDTO;
+import com.example.scheduling_service.domain.model.Session;
+
+public class SessionDisplayMapper {
+
+    public static SessionDisplayDTO toDTO(Session session){
+        return new SessionDisplayDTO(
+                session.getId().value(),
+                session.getRoomId().toString(),
+                session.getMovieId().toString(),
+                session.getSessionBeginTime(),
+                session.getSessionEndTime(),
+                session.getMovieDuration().toSeconds()
+        );
+    }
+
+}
