29e455eb77309a75123dbd0d1479de3cf7ddcfa0
CortexH <henrique.silveira.soares19988@gmail.com>
2025-07-17 00:11:21 -0300

    Remoção do 'movieException'. É desnecessário nesse cenário.

--- scheduling-service/src/main/java/com/example/scheduling_service/domain/exception/MovieException.java
+++
@@ -1,7 +0,0 @@
-package com.example.scheduling_service.domain.exception;
-
-public class MovieException extends RuntimeException {
-    public MovieException(String message) {
-        super(message);
-    }
-}
