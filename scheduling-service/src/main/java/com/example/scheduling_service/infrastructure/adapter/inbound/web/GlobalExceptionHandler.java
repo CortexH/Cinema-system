477962c8cd50881a96819807ef3c393d7f98c25a
CortexH <henrique.silveira.soares19988@gmail.com>
2025-07-18 00:18:19 -0300

    adição da tratação de erro para 'sessionException'

--- scheduling-service/src/main/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/GlobalExceptionHandler.java
+++ scheduling-service/src/main/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/GlobalExceptionHandler.java
@@ -1,10 +1,30 @@
 package com.example.scheduling_service.infrastructure.adapter.inbound.web;
 
+import com.example.scheduling_service.application.dto.response.GenericErrorResponse;
+import com.example.scheduling_service.domain.exception.SessionException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-
+    @ExceptionHandler(SessionException.class)
+    public ResponseEntity<GenericErrorResponse> handleSessionException(
+            SessionException ex, HttpServletRequest request
+    ){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new GenericErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                        ex.getMessage(),
+                        request.getRequestURI()
+                ));
+    }
 
 }
