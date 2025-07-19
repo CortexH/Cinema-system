ccf8f3674fe55ec1e113ffdbe40ce3588fc3db04
CortexH <henrique.silveira.soares19988@gmail.com>
2025-07-19 00:45:51 -0300

    Implementação dos testes unitários possíveis no cenário atual.
    endpoints: post '/api/v1/scheduler-sessions'
    
    Contemplando cenários de sucesso e cenários de falha.

--- scheduling-service/src/test/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/ScheduledSessionsIntegrationTests.java
+++ scheduling-service/src/test/java/com/example/scheduling_service/infrastructure/adapter/inbound/web/ScheduledSessionsIntegrationTests.java
@@ -21,6 +21,7 @@ import org.springframework.util.Assert;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -28,22 +29,24 @@ import java.util.UUID;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Transactional
 public class ScheduledSessionsIntegrationTests {
 
     private final MockMvc mockMvc;
     private final ObjectMapper objectMapper;
     private final SessionRepositoryJPA sessionRepository;
 
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
     @Test
-    @DisplayName("Deverá inserir uma sessão com sucesso")
-    @Transactional
-    void testInsertSession() throws Exception {
+    @DisplayName("Validar adicionar sessão")
+    void validateInsertSession() throws Exception {
 
         SessionRequestDTO data = new SessionRequestDTO(
                 UUID.randomUUID(), UUID.randomUUID(),
-                "2025-07-16T14:00:00",
-                "2025-07-16T17:00:00",
-                2700L, 540000L
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(3).format(dateTimeFormatter),
+                2700L, 8100L
         );
 
         Session requestSession = SessionRequestDTOMapper.toInbound(data);
@@ -67,41 +70,164 @@ public class ScheduledSessionsIntegrationTests {
     }
 
     @Test
-    @DisplayName("Validar remover sessão já agendada com replace desativado")
-    void testRemoveScheduledSession(){
-        SessionRequestDTO sessionRequestFirst = new SessionRequestDTO(
+    @DisplayName("Validar adicionar sessão sem enviar o campo 'movie_duration'")
+    void validateInsertSessionWithoutMovieDuration() throws Exception {
+
+        SessionRequestDTO data = new SessionRequestDTO(
                 UUID.randomUUID(), UUID.randomUUID(),
-                "2025-07-16T14:00:00",
-                "2025-07-16T17:00:00",
-                2700L, 540000L
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(3).format(dateTimeFormatter),
+                2700L, 8100L
         );
 
-        SessionRequestDTO sessionRequestLast = new SessionRequestDTO(
-                UUID.randomUUID(), UUID.randomUUID(),
-                "2025-07-16T17:00:00",
-                "2025-07-16T20:00:00",
-                2700L, 540000L
-        );
+        Session session = SessionRequestDTOMapper.toInbound(data);
+
+        String stringSession = objectMapper.writeValueAsString(data);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/scheduler-sessions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(stringSession))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.movie_duration").value(session.getMovieDuration().toSeconds()));
 
     }
 
-    void testAddConflictedSession(){
-        SessionRequestDTO sessionRequestFirst = new SessionRequestDTO(
+    @Test
+    @DisplayName("Validar remoção dos espaços de tempo vazios entre tempo de setup e tempo de filme")
+    void validateSessionSetupTimeGapFilling() throws Exception {
+        int addedSeconds = 25 * 60;
+        Duration movieDuration = Duration.ofMinutes(90);
+        Duration setupDuration = Duration.ofMinutes(45);
+        Duration totalSessionDuration = movieDuration.plus(setupDuration).plusSeconds(addedSeconds);
+
+        SessionRequestDTO data = new SessionRequestDTO(
                 UUID.randomUUID(), UUID.randomUUID(),
-                "2025-07-16T14:00:00",
-                "2025-07-16T17:00:00",
-                2700L, 540000L
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plus(totalSessionDuration).format(dateTimeFormatter),
+                setupDuration.toSeconds(), movieDuration.toSeconds()
         );
 
-        SessionRequestDTO sessionRequestLast = new SessionRequestDTO(
+        String request = objectMapper.writeValueAsString(data);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.setup_duration").value(setupDuration.toSeconds() + addedSeconds));
+    }
+
+    // FAIL SCENARIOS
+
+    @Test
+    @DisplayName("Validar adicionar sessões com conflito entre horários")
+    void validateAddConflictedSession() throws Exception {
+
+        SessionRequestDTO session = new SessionRequestDTO(
                 UUID.randomUUID(), UUID.randomUUID(),
-                "2025-07-16T15:00:00",
-                "2025-07-16T18:00:00",
-                2700L, 540000L
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(3).format(dateTimeFormatter),
+                2700L, 8100L
         );
 
+        SessionRequestDTO sameTimeSession = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(3).format(dateTimeFormatter),
+                2700L, 8100L
+        );
 
+        SessionRequestDTO conflictedEndTimeSession = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                LocalDateTime.now().minusHours(2).format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(1).format(dateTimeFormatter),
+                2700L, 8100L
+        );
 
+        SessionRequestDTO conflictedStartTimeSession = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                LocalDateTime.now().plusHours(2).format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(5).format(dateTimeFormatter),
+                2700L, 8100L
+        );
+
+        SessionRequestDTO endTimeBeforeStartTimeSession = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                LocalDateTime.now().plusHours(6).format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(5).format(dateTimeFormatter),
+                2700L, 8100L
+        );
+
+        String sessionRequest = objectMapper.writeValueAsString(session);
+        String sameTimeRequest = objectMapper.writeValueAsString(sameTimeSession);
+        String conflictedEndTimeRequest = objectMapper.writeValueAsString(conflictedEndTimeSession);
+        String conflictedStartTimeRequest = objectMapper.writeValueAsString(conflictedStartTimeSession);
+        String endTimeBeforeStartTimeRequest = objectMapper.writeValueAsString(endTimeBeforeStartTimeSession);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(sessionRequest)).andExpect(MockMvcResultMatchers.status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(sameTimeRequest)).andExpect(MockMvcResultMatchers.status().isConflict());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(conflictedEndTimeRequest)).andExpect(MockMvcResultMatchers.status().isConflict());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(conflictedStartTimeRequest)).andExpect(MockMvcResultMatchers.status().isConflict());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(endTimeBeforeStartTimeRequest)).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Validar adicionar uma sessão com tempo de setup maior que o tempo total da sessão")
+    void validateSetupDurationGreaterThanSessionDuration() throws Exception {
+        SessionRequestDTO data = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(1).format(dateTimeFormatter),
+                (long) (120 * 60), 0L
+        );
+
+        String request = objectMapper.writeValueAsString(data);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/scheduler-sessions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(request))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
+
+    @Test
+    @DisplayName("Validar adicionar uma sessão com tempo de filme maior que o tempo total da sessão")
+    void validateMovieDurationGreaterThanSessionDuration() throws Exception {
+        SessionRequestDTO data = new SessionRequestDTO(
+                UUID.randomUUID(), UUID.randomUUID(),
+                LocalDateTime.now().format(dateTimeFormatter),
+                LocalDateTime.now().plusHours(1).format(dateTimeFormatter),
+                0L, (long) (120 * 60)
+        );
+
+        String request = objectMapper.writeValueAsString(data);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/scheduler-sessions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
 }
